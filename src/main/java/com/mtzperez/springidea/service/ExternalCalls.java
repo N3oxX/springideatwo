c23c0ec9ab3aae00a898d2931ed2642d1a9b10e3
David Martinez <david.martinez@nter.es>
2020-03-09 17:17:13 +0100

    bikes service

--- src/main/java/com/mtzperez/springidea/service/ExternalCalls.java
+++ src/main/java/com/mtzperez/springidea/service/ExternalCalls.java
@@ -4,21 +4,18 @@ import com.mtzperez.springidea.model.dto.CarDto;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 import javax.validation.Valid;
 import java.util.List;
 
 @FeignClient(name="springidea")
-
+@RequestMapping("cars")
 public interface ExternalCalls {
 
-    @GetMapping("/cars")
     public List<CarDto> getAll();
 
-    @PostMapping("/cars")
     public CarDto create(@Valid @RequestBody CarDto car);
 }
